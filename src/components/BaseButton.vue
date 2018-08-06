5987fe2bb50a9a2bb8e3b37db5478edc2f99ee6b
Mai <jm8259@JMAI01DM-8.local>
2018-08-06 16:27:12 -0400

    style changes to primary, secondary, and destructive buttons in the .scss file, and a change to basebutton.vue file to only show focus style when user is tabbing

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -11,6 +11,16 @@
 <script lang="ts">
 import Vue from "vue"
 
+function handleFirstTab(e) {
+    if (e.keyCode === 9) { // tab key, user is a keyboard user
+        document.body.classList.add('user-is-tabbing');
+        window.removeEventListener('keydown', handleFirstTab);
+    }
+}
+
+window.addEventListener('keydown', handleFirstTab);
+
+
 export default Vue.extend({
   name: "BaseButton",
   props: {
