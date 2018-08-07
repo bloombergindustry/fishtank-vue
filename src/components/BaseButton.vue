9c12e5a76b77169a76968145ff266b2ad1b77c7e
Mai <jm8259@JMAI01DM-8.local>
2018-08-07 11:42:40 -0400

    added listener to basebuttons.vue to hide focus state on user mouse click

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -12,10 +12,19 @@
 import Vue from "vue"
 
 function handleFirstTab(e) {
-    if (e.keyCode === 9) { // tab key, user is a keyboard user
-        document.body.classList.add('user-is-tabbing');
-        window.removeEventListener('keydown', handleFirstTab);
-    }
+  if (e.keyCode === 9) {
+    document.body.classList.add('user-is-tabbing');
+    
+    window.removeEventListener('keydown', handleFirstTab);
+    window.addEventListener('mousedown', handleMouseDownOnce);
+  }
+}
+
+function handleMouseDownOnce() {
+  document.body.classList.remove('user-is-tabbing');
+  
+  window.removeEventListener('mousedown', handleMouseDownOnce);
+  window.addEventListener('keydown', handleFirstTab);
 }
 
 window.addEventListener('keydown', handleFirstTab);
