6598acb2b499d9179a1080e1d658eea8864826eb
Amirah Chamble <Amirah.Chamble@users.noreply.github.com>
2018-09-18 16:28:42 -0400

    feat| Added basic outline of FAB button to vue components. Basic functionality is present

--- dev/views/Buttons.vue
+++ dev/views/Buttons.vue
@@ -2,58 +2,64 @@
   <div>
     <div><ButtonPrimary>Button Primary</ButtonPrimary></div>
     <br>
-    <ButtonPrimary :block="true">Button Primary</ButtonPrimary>
-    <br>
     <div><ButtonSecondary>Button Secondary</ButtonSecondary></div>
     <br>
     <div><ButtonDestructive>Button Destructive</ButtonDestructive></div>
     <br>
     <div><ButtonOutline>Button Outline</ButtonOutline></div>
     <br>
-    <div><ButtonGradient 
-      :color-direction="colorDirection" 
-      :gradient-start="colorStart" 
-      :gradient-end="colorEnd" >Button Gradient</ButtonGradient></div>
-    <br>
-    <div><ButtonGradient 
-      :color-direction="colorDirection" 
-      :gradient-start="colorStart" 
-      :gradient-end="colorEnd" 
-      :block="true">Button Gradient</ButtonGradient></div> 
-    <br>
-    <div><ButtonGradient 
-      :color-direction="colorDirection" 
-      gradient-start="red"
-      gradient-end="yellow" >Button Gradient</ButtonGradient></div>
-    <br>
-    <div><ButtonGradient  >Button Gradient</ButtonGradient></div>
-    <br>
     <div><ButtonWhite>Button White</ButtonWhite></div>
     <br>
+    <div><ButtonGradient :color-direction="colorDirection" :gradient-start="colorStart" :gradient-end="colorEnd" >Button Gradient</ButtonGradient></div>
+    <br>
+
+    <div>
+      <ButtonFAB v-bind:fab-option="fabLinks" > 
+        +
+         
+      </ButtonFAB></div>
+
+    <br>
+
+    
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-
-import { ButtonPrimary,ButtonSecondary,ButtonDestructive, ButtonOutline, ButtonGradient , ButtonWhite} from '@/index'
+import { ButtonPrimary,ButtonSecondary,ButtonDestructive, ButtonWhite, ButtonOutline, ButtonGradient, ButtonFAB } from '@/index'
 
 export default Vue.extend({
   components: {
     ButtonPrimary,
     ButtonSecondary,
     ButtonDestructive,
-    ButtonWhite,
     ButtonOutline,
-    ButtonGradient
+    ButtonWhite,
+    ButtonGradient,
+    ButtonFAB
   },
-  data: function(){
+  data: 
+  function(){
     return{
+      //Relevant to ButtonGradient
       colorStart : "#0018AB",
       colorEnd : "#9933CC",
-      colorDirection: "to right"
+      colorDirection: "to right",
+
+      //Relevant to ButtonFAB
+      fabLinks:[
+        {icon: '1'},
+        {icon: '2'},
+        {icon: '3'},
+      ],
     }
-  }
+  },
+  
+
+
+
+
 })
 </script>
 
---
+++ src/components/ButtonFAB.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="ft-button--fab-wrapper">
+
+  <BaseButton v-bind="$attrs" v-on="$listeners" class="ft-button--fab">
+    <p> <slot/> </p>
+  </BaseButton>
+
+  <ul>
+   <li v-for="fablink in fabOption">
+   <a href="#"> {{fablink.icon}} </a>
+   </li>                                                                   
+  </ul>
+
+  </div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import BaseButton from "./BaseButton.vue"
+
+export default Vue.extend({
+  name: "ButtonFAB",
+  components: {
+    BaseButton
+  },
+  props:['fabOption'],   
+  computed:{
+    
+  }
+})
+
+</script>
