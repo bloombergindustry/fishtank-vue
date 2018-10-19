287402a42912ad6689ae4056c90c25fffb295450
Brian Akpa <ba105445@bgov.com>
2018-10-19 08:42:15 -0400

    feat: updated package.json to reference fishtank vue-treeselect

---
+++ dev/views/TreeSelect.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <FishTankTreeSelect
+      :options="treeArr"
+      :max-height="null"
+      :hide-control="true"
+      :dropdownpopover="false"
+      :multiple="true"
+      :collapse-menu-padding="true"
+      v-model="treeVal" />
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import {FishTankTreeSelect} from '@/index'
+
+export default Vue.extend({
+  components:{
+    FishTankTreeSelect
+  },
+  data: function(){
+    return {
+      treeVal:[],
+      treeArr:[{
+        label:"label",
+        id:"1",
+        children:[
+          {
+            label:"label child",
+            id:"2",
+            children:[
+              {
+                label:"label child",
+                id:"4"
+              }
+            ]
+          },
+          {
+            label:"label child",
+            id:"3",
+            children:[]
+          }
+        ]
+      }]
+    }
+  }
+})
+</script>
