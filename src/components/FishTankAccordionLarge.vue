f1a7eea0cc38287036040cf69b5a3eb1c5138c04
Amirah Chamble <Amirah.Chamble@users.noreply.github.com>
2019-01-10 11:22:25 -0500

    feat: moved scss styles within accordion component

--- src/components/FishTankAccordionLarge.vue
+++ src/components/FishTankAccordionLarge.vue
@@ -1,33 +1,33 @@
 <template>
     <div 
-        :class="[disabled ? 'ft-accordion-container-gray' : 'ft-accordion-container']"
+        :class="[disabled ? 'accordion-container-gray' : 'accordion-container']"
     > 
         <div 
-            class="ft-accordion-wrapper-lg"
+            class="accordion-wrapper-lg"
             @click="toggle"
         >
-            <div class="ft-accordion-heading-wrapper">
+            <div class="accordion-heading-wrapper">
                 <p 
-                    class="ft-accordion-heading"
+                    class="accordion-heading"
                     role="heading"
                     aria-level="3"
                 > 
                     {{ heading }} 
                 </p>
                 <p 
-                    class="ft-accordion-sub-heading"
+                    class="accordion-sub-heading"
                     v-if="subheading"
                     role="heading"
                     aria-level="4"
                 > {{ subheading }} </p>
             </div>
             <ChevronUp24 
-                :class="[ visible ? 'ft-accordion-svg-down' : 'ft-accordion-svg-up' ]"
+                :class="[ visible ? 'accordion-svg-down' : 'accordion-svg-up' ]"
                 aria-label="collapse / expand content"
             />
         </div>
         <div 
-            class="ft-accordion-panel"
+            class="accordion-panel"
             v-show="visible"
         >
             <slot/>
@@ -80,7 +80,114 @@
     }
 </script>
 
-<style lang="scss">
+<style  lang="scss">
+    @import "../../node_modules/@fishtank/colors/dist/index";
+    @import "../../node_modules/@fishtank/type/dist/index";
+
+    *{
+        margin: 0px;
+    }
+
+    .accordion-container{
+        display: flex;
+        flex-direction: column;
+        margin: 12px;
+    }
+
+    .accordion-wrapper-lg{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        padding: 12px;
+        border-bottom: 1px solid $color-gray-lighter;
+    }
+
+    .accordion-container .accordion-wrapper-lg{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        padding: 12px;
+        border-bottom: 1px solid $color-gray-lighter;
+
+        &:hover{
+
+            .accordion-heading-wrapper{
+
+                .ft-accordion-heading{
+                    color: $color-black;
+                }
+
+                .accordion-sub-heading{
+                    color: $color-black;
+                }
+
+            }
+
+            svg{
+                color: $color-black;
+            }
+
+        }
+
+    }
+
+    .accordion-heading-wrapper{
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+    }
+
+    .accordion-heading{
+        color: $color-gray-dark;
+        font-size: $fontsize-base-lg;
+        font-weight: $fontweight-regular;
+        line-height: $lineheight-base-lg;
+        letter-spacing: $letterspacing-base-lg;
+    } 
+
+    .accordion-sub-heading{
+        color: $color-gray;
+        font-size: $fontsize-base-sm;
+        font-weight: $fontweight-regular;
+        line-height: $lineheight-base-sm;
+        letter-spacing: $letterspacing-small;
+    }
+
+    svg{
+        color: $color-gray;      
+    }
+
+    .accordion-panel{
+        padding: 0px
+    }
+
+    .accordion-container-gray{
+        display: flex;
+        flex-direction: column;
+        margin: 12px;
+    }
+
+    .accordion-container-gray .accordion-heading{
+        color: $color-gray-lighter;
+    }
+
+    .accordion-container-gray .accordion-sub-heading{
+        color: $color-gray-lighter;
+    }
+    .accordion-container-gray svg{
+        color: $color-gray-lighter;
+    }
+
+    .accordion-svg-up{
+        transition: 0.3s ease-in-out;
+        transform: rotate(180deg);
+    }
+    .accordion-svg-down{
+        transition: 0.3s ease-in-out;
+        transform: rotate(0deg);
+    }
 
 </style>
 
