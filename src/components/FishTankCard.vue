6ffcb8c0bf893b968b7c5b086ec055f1ef3afc4e
Amirah Chamble <Amirah.Chamble@users.noreply.github.com>
2019-01-10 15:34:58 -0500

    feat: removed Cards scss files and added styles within Cards  component

--- src/components/FishTankCard.vue
+++ src/components/FishTankCard.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="ft-card">
+  <div class="card">
     <div
       v-if="branded"
       :class="branded"
-      class="ft-card__brand-stripe"
+      class="card__brand-stripe"
     />
     <div 
-      class="ft-card__container"
+      class="card__container"
     >
       <slot
-        :headerClass="'ft-card__heading'"
+        :headerClass="'card__heading'"
         name="heading"
       />
       <div 
         v-if="!$slots.heading && heading"
-        class="ft-card__heading"
+        class="card__heading"
         role="heading"
         aria-level="3"
       >
@@ -22,7 +22,7 @@
       </div>
 
       <div
-        class="ft-card__body"
+        class="card__body"
       >
         <slot/>
       </div>
@@ -48,3 +48,63 @@ export default Vue.extend({
   }
 })
 </script>
+
+<style lang="scss">
+  @import '../styles/mixins';
+  @import "../../node_modules/@fishtank/colors/dist/index";
+  @import "../../node_modules/@fishtank/type/dist/index";
+
+  .card__container {
+    box-sizing: border-box;
+    border-radius: 2px;
+    box-shadow: 0 2px 5px 0 rgba(0,0,0,0.3);
+  }
+
+  .card__brand-stripe {
+    border-radius: 2px 2px 0 0;
+    height: 4px;
+  }
+
+  .card__brand-stripe + .card__container {
+    .card__heading {
+      border-radius: 0;
+    }
+
+    .card__body {
+      border-top: none;
+      border-top-right-radius: 0;
+      border-top-left-radius: 0;
+    }
+  }
+
+  .card__heading {
+    border: 1px solid $color-gray-lighter;
+    border-radius: 2px 2px 0 0;
+    padding: $baseline * 3;
+    background-color: $color-gray-lightest;
+    font-size: $fontsize-base-lg;
+    line-height: $lineheight-base-lg;
+    font-family: $font-primary;
+    font-weight: $fontweight-semi;
+    letter-spacing: $letterspacing-base-lg;
+  } 
+
+  .card__body {
+    padding: $baseline * 3;
+    border-radius: 2px;
+    background-color: $color-white;
+    border: 1px solid $color-gray-lighter;
+  }
+
+  .card__heading + .card__body {
+    border-top: none;
+    border-top-right-radius: 0;
+    border-top-left-radius: 0;
+  }
+
+  .card + .card {
+    margin-top: $baseline * 6;
+  }
+
+</style>
+
