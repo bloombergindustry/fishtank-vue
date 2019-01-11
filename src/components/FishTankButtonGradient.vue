9f02496896a5195a254202de8369406b88d91663
Amirah Chamble <Amirah.Chamble@users.noreply.github.com>
2019-01-11 16:08:19 -0500

    feat: updated scss for buttons Gradients, Outline Dark, and Outline Dark Small

--- src/components/FishTankButtonGradient.vue
+++ src/components/FishTankButtonGradient.vue
@@ -1,12 +1,12 @@
 <template>
   <a 
-    :class="{ 'ft-button--block': block }"
+    :class="{ 'button--block': block }"
     :style="returnGradientStyle" 
-    class="ft-button--wrapper">
+    class="button--wrapper">
     <FishTankBaseButton 
       v-bind="$attrs"
       :block="block"
-      class="ft-button--gradient"
+      class="button--gradient"
       v-on="$listeners"
     >
       <slot/>
@@ -64,3 +64,44 @@ export default Vue.extend({
 })
 
 </script>
+
+<style lang="scss">
+
+  @import '../styles/mixins';
+  @import "../../node_modules/@fishtank/colors/dist/index";
+  @import "../../node_modules/@fishtank/type/dist/index";
+
+  .button--block {
+    display: block;
+    width: 100%;
+  }
+
+  .button--wrapper{
+    padding: 2px ;
+    border-radius: 4px;
+    box-sizing: border-box;
+    display: inline-block;
+  }
+
+  .button--gradient {
+    border:none;
+    background-color: $color-white;
+    color: $color-gray;
+
+    &:hover {
+      background: transparent;
+      color: $color-white;
+    }
+    &:disabled {
+      opacity: 0.4;
+      &:hover {
+        background-color: $color-gray;
+      }
+    }
+    &:active {
+      background-color:rgba(0,0,0,0.3);
+      border-color: rgba(0,0,0,0.3);
+    }
+  }
+
+</style>
--- src/components/FishTankButtonOutlineDark.vue
+++ src/components/FishTankButtonOutlineDark.vue
@@ -1,7 +1,7 @@
 <template>
   <FishTankBaseButton
     v-bind="$attrs"
-    class="ft-button--outline-dark"
+    class="button--outline-dark"
     v-on="$listeners"
   >
     <slot/>
@@ -24,3 +24,35 @@ export default Vue.extend({
   }
 })
 </script>
+
+<style lang="scss">
+
+  @import '../styles/mixins';
+  @import "../../node_modules/@fishtank/colors/dist/index";
+  @import "../../node_modules/@fishtank/type/dist/index";
+
+  .button--outline-dark {
+    background-color: transparent;
+    border-color: $color-white;
+    border-width: 2px;
+    color: $color-white;
+
+    &:hover {
+      color: $color-gray-dark;
+      background-color: $color-white;
+    }
+    &:disabled {
+      color: $color-gray-dark;
+      border-color: $color-gray-dark;
+
+      &:hover{
+        background-color: transparent;
+      }
+    }
+    &:active {
+      background-color: $color-secondary-darker;
+      border-color: $color-secondary-darker;
+    }
+  }
+
+</style>
