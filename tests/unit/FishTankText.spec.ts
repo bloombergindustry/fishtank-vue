4d2a0a84c3ce3690b0d175a8c44d7628274527d8
Brian Akpa <ba105445@bgov.com>
2019-03-07 13:10:07 -0500

    fixing merge errors; roll back to vue@2.5.17

--- tests/unit/FishTankText.spec.ts
+++ tests/unit/FishTankText.spec.ts
@@ -1,7 +1,6 @@
 import { mount, shallowMount as  shallow } from '@vue/test-utils'
 import { expect } from "chai"
 import FishTankText from '@/components/FishTankText.vue'
-<<<<<<< HEAD
 const getCleanClassNames = function(classList:any):Array<string>{
   let classArray = Array.from(classList)
   let cleanClasses = classArray.map((c:any)=>{
@@ -9,9 +8,6 @@ const getCleanClassNames = function(classList:any):Array<string>{
   })
   return cleanClasses
 }
-=======
-
->>>>>>> 483f7d2cc6e3091bac344bb0f2b7b22a6073a568
 describe('FishTankText.vue', () => {
   const propsData = {
     prop1: '',
@@ -47,14 +43,7 @@ describe('FishTankText.vue', () => {
   it('contains default classes of alignLeft, antialiased, colorBlack, fontSizeBaseLg and primary', () =>{
     const defaultClasses = [ 'alignLeft','antialiased','colorBlack','fontSizeBaseLg','primary' ]
     const wrapper = mountInput()
-<<<<<<< HEAD
     let cleanClasses = getCleanClassNames(wrapper.element.classList)
-=======
-    let classArray = Array.from(wrapper.element.classList)
-    let cleanClasses = classArray.map((c:String)=>{
-      return c.split('_')[1]
-    })
->>>>>>> 483f7d2cc6e3091bac344bb0f2b7b22a6073a568
     cleanClasses.forEach(c=>{
       expect(defaultClasses.indexOf(c) >-1)
     })
