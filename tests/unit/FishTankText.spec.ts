0c8d9138ce3843636df0f03ed3351e5608585652
Brian Akpa <ba105445@bgov.com>
2019-03-07 13:03:29 -0500

    fix: created cleanClasses function for text test

--- tests/unit/FishTankText.spec.ts
+++ tests/unit/FishTankText.spec.ts
@@ -1,7 +1,13 @@
 import { mount, shallowMount as  shallow } from '@vue/test-utils'
 import { expect } from "chai"
 import FishTankText from '@/components/FishTankText.vue'
-
+const getCleanClassNames = function(classList:any):Array<string>{
+  let classArray = Array.from(classList)
+  let cleanClasses = classArray.map((c:any)=>{
+    return c.split('_')[1]
+  })
+  return cleanClasses
+}
 describe('FishTankText.vue', () => {
   const propsData = {
     prop1: '',
@@ -37,10 +43,7 @@ describe('FishTankText.vue', () => {
   it('contains default classes of alignLeft, antialiased, colorBlack, fontSizeBaseLg and primary', () =>{
     const defaultClasses = [ 'alignLeft','antialiased','colorBlack','fontSizeBaseLg','primary' ]
     const wrapper = mountInput()
-    let classArray = Array.from(wrapper.element.classList)
-    let cleanClasses = classArray.map((c:String)=>{
-      return c.split('_')[1]
-    })
+    let cleanClasses = getCleanClassNames(wrapper.element.classList)
     cleanClasses.forEach(c=>{
       expect(defaultClasses.indexOf(c) >-1)
     })
