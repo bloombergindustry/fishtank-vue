6ce9f35d4ce68e18a3069e2c57cc503cb6510380
Brian Akpa <ba105445@bgov.com>
2019-03-18 17:05:40 -0400

    additional dev work

--- src/components/FishTankSelect.vue
+++ src/components/FishTankSelect.vue
@@ -1,273 +1,198 @@
 <template>
-  <fish-tank-box 
-    position="relative" 
-    @blur.native="close" 
-    @mouseleave.native="close">
-    <fish-tank-box
-      :id="instanceId" 
-      color="grayLightest" 
-      :padding="2" 
-      display="flex" 
-      :class="[$style.wrapper, 'a11y']" 
-      :tabindex="(displaySelect ? null : 0)" 
-      aria-haspopup="listbox"
-      @click.native="toggle"
-      :aria-expanded="(displaySelect ? true : null)"
-      @keyup.enter.native="openSelect"
-      :aria-label="selected.label">
-      <fish-tank-box>
-        <fish-tank-text 
-          color="grayDark">{{ selected.label }} 
-        </fish-tank-text>
-      </fish-tank-box>
-      <fish-tank-box 
-        v-if="!noItems" 
-        align="right">
-        <CaretDown24 
-          v-if="!displaySelect"/>
-        <CaretUp24 
-          v-if="displaySelect"/>
-      </fish-tank-box>
-    </fish-tank-box>
-    <fish-tank-box
-      v-if="displaySelect"
-      :class="[$style.selectOpen]" 
-      :aria-expanded="`${displaySelect}`"
-      :aria-labelledby="instanceId">
-      <fish-tank-box
-        v-for="(item, index) in items" 
-        :key="index"
-        color="white"
-        role="listbox"
-        @keyup.up.native="prevItem"
-        @keyup.down.native="nextItem"
-        @click.native="handleChange(item)"
-        @focus.native="accessibleHandleChange(item)"
-        @keyup.enter.native="close()"
-        @keydown.tab.prevent.native="close()"
-        @blur.native="close()">
-        <a
-          :ref="item.label"
-          :aria-selected="(selected.label === item.label ? true: null)"
-          :class="[$style.selectOption, {[$style.selectedOption]:(selected.label === item.label)}]"
-          :padding="2"
-          :tabindex="(selected.label === item.label ? 0: -1)"
-          
-          role="option">
-          <fish-tank-box
-            :padding="2">
-            <div class="option-text">
-              <fish-tank-text 
-              color="grayDark">
-              {{ item.label }}</fish-tank-text>
-            </div>
-          </fish-tank-box>
+  <div 
+    class="select" 
+    :name="name" 
+    :orientation="orientation" 
+    @mouseleave="opened=false">
+    <slot 
+      v-if="label" 
+      name="label">
+      {{ label }}
+    </slot>
+    <div class="positionWrap">
+      <button 
+        class="selected a11y" 
+        aria-haspopup="listbox"
+        :placeholder="!value" 
+        @click="opened=!opened">
+        <box 
+          class="selectedWrap" 
+          display="flex" 
+          justify-content="between" 
+          :padding=2> 
+          <ftext
+            color="grayDark">
+            {{ displayLabel }}
+          </ftext>
+          <caretdown 
+            class="caret"
+            v-if="!opened"/>
+          <caretdown
+            class="caret"
+            v-if="opened  "/>
+        </box>
+      </button>
+      
+      <div 
+        v-if="opened" 
+        class="items a11y-within" 
+        role="listbox">
+        <a 
+          v-for="(item, index) in items" 
+          :key="index" 
+          tabindex="0" 
+          @keyup.enter="$emit('change', item.value, $nextTick(()=>opened=false))" 
+          @click="$emit('change', item.value); opened = false"
+          @blur="closeDropdown(items, index)"
+          class="listItem">
+          <box :padding=2>
+            <ftext>{{ item.label }}</ftext>
+          </box>
         </a>
-      </fish-tank-box>
-    </fish-tank-box>
-  </fish-tank-box>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-  import FishTankText from './FishTankText.vue'
-  import FishTankBox from './FishTankBox.vue'
-  
-  import {
-    CaretDown24,
-    CaretUp24
-  } from '@fishtank/icons-vue'
-  import mixins from '@/util/a11y'
-  import {uuid} from '@/util/uuid'
+import FishTankText from './FishTankText.vue'
+import FishTankBox from './FishTankBox.vue'
+import { CaretDown24 } from '@fishtank/icons-vue'
+import a11y from '../util/a11y.ts'
+/**
+ * Change event.
+ *
+ * @event change
+ * @type {string}
+ */
 
-  const id = ''
 /**
  * Custom dropdown menu
  *
  * Style variables:
  * --border-color
+ * --selector-height
+ * --selector-width
+ * --border-radius
  * --hover-background-color
  * --dropdown-background-color
  * --dropdown-icon-color
+ * --text-color
+ * --font-size
  */
-
 export default {
   name: 'FishTankSelect',
+  mixins:[
+    a11y
+  ],
   components:{
-    FishTankText,
-    FishTankBox,
-    CaretDown24,
-    CaretUp24
+    // text is a reserved name, so using ftext
+    ftext: FishTankText,
+    box: FishTankBox,
+    caretdown: CaretDown24
   },
-  mixins:[mixins],
-  beforeCreate () {
-    this.instanceId = `FishTankSelect_${uuid()}`
-  },
-
   props: {
     /**
      * Array of dropdown items
      */
     items: {
       type: Array,
-      default () {
-        return []
-      },
-      description:"Array of dropdown items"
+      default: () => []
     },
+
     /**
-     * Current selected item
+     * Input label
      */
-    selected: {
-      type: Object,
-      default () {
-        if (this.items.length > 0) {
-          return this.items[0]
-        } else { // if no items or selected value are passed
-          return {label: 'No items'}
-        }
-      },
-      description: "Current selected item"
-    }
+    label: String,
+
+    /**
+     * Element name (for forms use)
+     */
+    name: String,
+
+    /**
+     * Orientation of label/input, ie ttb/rtl/ltr
+     */
+    orientation: {
+      type: String,
+      default: 'ttb'
+    },
+
+    /**
+     * Placeholder text
+     */
+    placeholder: String,
+
+    /**
+     * Current selected item value
+     */
+    value: String | Number
+  },
+  model: {
+    event: 'change',
+    prop: 'value'
   },
   data () {
     return {
-      displaySelect: false
+      opened: false
     }
   },
-  methods: {
-    handleChange (newItem) {
-      this.close()
-      /**
-       * Change event.
-       *
-       * @event change
-       * @type {object}
-       */
-      this.$emit('change', { new: newItem, old: this.selected })
-    },
-    accessibleHandleChange (newItem) {
-      // this.close()
-      /**
-       * Change event.
-       *
-       * @event change
-       * @type {object}
-       */
-      this.$emit('change', { new: newItem, old: this.selected })
-    },
-    /**
-     * Toggle (open/close) dropdown list
-     *
-     * @public
-     */
-    toggle () {
-      this.displaySelect = !this.displaySelect
-    },
-    /**
-     * Close dropdown list
-     *
-     * @public
-     */
-    close () {
-      this.displaySelect = false
-    },
-    /**
-     * Arrow navigate to the next item in the dropdown list
-     *
-     * @public
-     */
-    nextItem () {
-      this.$nextTick(function(){
-        let ind = 0
-        this.items.forEach((el, i) => {
-          if (el.label === this.selected.label){
-            ind = i
-          }
-        })
-        if (ind === (this.items.length - 1)){
-          this.$emit('change', { new: this.items[0], old: this.selected })
-        } else {
-          this.$emit('change', { new: this.items[ind+1], old: this.selected })
-        }
-      }) 
-    },
-    /**
-     * Arrow navigate to the previous item in the dropdown list
-     *
-     * @public
-     */
-    prevItem () {
-      this.$nextTick(function(){
-        let ind = 0
-        this.items.forEach((el, i) => {
-          if (el.label === this.selected.label){
-            ind = i
-          }
-        })
-        if (ind === 0){
-          this.$emit('change', { new: this.items[(this.items.length - 1)], old: this.selected })
-        } else {
-          this.$emit('change', { new: this.items[ind-1], old: this.selected })
-        }
-      }) 
-    },
-    /**
-     * open select list
-     *
-     * @public
-     */
-    openSelect () {
-      this.displaySelect = true
-      if (event.key === 'Enter'){
-        this.$nextTick(function(){
-          let el = this.$refs[this.selected.label][0]
-          el.focus()
-        })
-      }
-    },
-  },
   computed: {
-    noItems () {
-      return this.items.length === 0
+    displayLabel () {
+      return this.value ? this.items.find(x => x.value === this.value).label : this.placeholder
+    }
+  },
+  methods:{
+    closeDropdown: function(items, index){
+      if ((items.length - 1) === index){
+        this.opened=false
+      }
     }
   }
 }
 </script>
-<style lang="scss">
+
+<style scoped lang="scss">
+@import '../styles/mixins';
 @import '../../node_modules/@fishtank/colors/dist/index.custom-properties';
-.user-is-tabbing .a11y:focus{
-    box-shadow: 0 0 0 2px var(--color-selected);
-  }
-</style>
 
-<style module lang="scss">
-  @import '../../node_modules/@fishtank/colors/dist/index.custom-properties';
-  @import '../../node_modules/@fishtank/space/dist/index.custom-properties';
-
-  .wrapper{
-    border:  1px solid var(--color-gray-lighter);
-    position: relative;
-    outline: 0;
+.select{
+  --select-background: var(--color-secondary);
+  --selected-wrap: var(--color-secondary-lighter);
+}
+.positionWrap{
+  position: relative;
+}
+.selected{
+  background-color:transparent;
+  border: 0;
+  width: 100%;
+  text-align: left;
+  padding: 0;
+  outline: 0;
+}
+body.user-is-tabbing .a11y:focus, body.user-is-tabbing .a11y-within:focus-within{
+  box-shadow: 0 0 0 2px $color-selected;
+}
+.selectedWrap{
+  background-color: var(--select-background);
+  border: 1px solid #C5CACD;
+}
+.items {
+  border: 1px solid var(--border-color, #C5CACD);
+  border-radius: 2px;
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,0.3);
+  background-color: var(--dropdown-background-color, #FFFFFF);
+  position: absolute;
+  min-width: 100%;
+  z-index: 100;
+  top:0;
+}
+.listItem{
+  display: block;
+  &:hover, &:focus {
+    background-color: var(--hover-background-color, #E7F5FB);
+    outline: none;
   }
-
-  .selectOpen {
-    z-index: 100;
-    position: absolute;
-    width: 100%;
-    transition: transform 0.4s;
-    transform-style: preserve-3d;
-    border:  1px solid var(--color-gray-lighter);
-    margin-top: -45px;
-    outline: 0;
-    &:focus{
-      background-color: red;
-    }
-  }
-  .selectOption{
-    display:block;
-    outline: 0;
-  }
-  .selectOption:hover, .selectOption:focus, .selectedOption{
-    background-color: var(--color-selected-lightest);
-  }
+}
 </style>
