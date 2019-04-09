d37a2801876b9b6d3535893c110922bbbb9d6282
Brian Akpa <ba105445@bgov.com>
2019-04-09 14:06:21 -0400

    feat: Merged BLAW RadioList and FishTankRadio into FishTankRadioList; Provided accessibility structure to allow radio groups, tabbing between groups, and arrow navigation between options for keyboard navigation, and screen reader support

---
+++ src/components/FishTankRadioList.vue
@@ -0,0 +1,122 @@
+<template>
+  <fieldset :id="`radio-list-${identifier}`" class="RadioList" :name="name">
+    <legend v-if="label">{{label}}</legend>
+    <div class="items" role="radiogroup">
+      <div :disabled="item.disabled" v-for='(item, index) in items' :key='index' class="wrap">
+        <radio 
+          v-model="value"
+          :id="`radio-${identifier}-${item.value}`"
+          :value="item.value" 
+          :disabled="item.disabled"
+          @click="handleClick" 
+          :label="item.label"
+          :name="label">
+            <slot v-if="item.slot" :name="item.slot">{{item.label}}</slot>
+          </radio>
+      </div>
+    </div>
+  </fieldset>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import radio from './FishTankRadio.vue' 
+/** Triggered when value is changed
+  * @event change
+  * @type {Event}
+  */
+export default Vue.extend({
+  name: 'RadioList',
+  components:{
+    radio
+  },
+  model: {
+    prop: 'value',
+    event: 'change'
+  },
+  props: {
+    /**
+     * Available items to display
+     * @param {Object[]} [items=[]] - Child object
+     * @param {Boolean} [items[].disabled] - Disabled state
+     * @param {String} [items[].label] - Child label
+     * @param {String} [items[].slot] - Child label slot name
+     * @param {String} [items[].value] - Child value
+     */
+    items: Array,
+
+    /**
+     * Input label
+     */
+    label: String,
+
+    /**
+     * Input elements name
+     */
+    name: String,
+
+    /**
+     * Current value
+     */
+    value: String,
+    
+    /**
+     * Block orientation; inline by defaul
+     */
+    block:Boolean
+  },
+  data () {
+    return {
+      /**
+       * Unique identifier to handle issues with duplicate ids when multiple RadioLists are present
+       */
+      identifier: (Math.random() * 10000).toFixed(0).toString(),
+    }
+  },
+  methods: {
+    handleClick (e:MouseEvent) {
+      if (e && e.target){
+        this.$emit('change', (e.target as HTMLInputElement).value)
+      }
+      e.preventDefault()
+    },
+  }
+})
+</script>
+
+<style scoped lang='scss'>
+fieldset {
+  margin-inline-start: 0;
+  margin-inline-end: 0;
+  padding-block-start: 0;
+  padding-inline-start: 0;
+  padding-inline-end: 0;
+  padding-block-end: 0;
+  min-inline-size: min-content;
+  border-width: 0;
+  border-style: none;
+  border-color: transparent;
+  border-image: hidden;
+}
+.RadioList {
+  .items {
+    align-items: center;
+    display: flex;
+    padding:0;
+    margin: 0;
+    border:0;
+  }
+  .wrap{
+    position: relative;
+    margin-right: 25px;
+  }
+  // Internet explorer hack
+  // & /deep/ {
+  //   @media all and (-ms-high-contrast: none), (-ms-high-contrast: active) {
+  //     input[type=radio]+label:after {
+  //       top: 3px;
+  //     }
+  //   }
+  // }
+}
+</style>
