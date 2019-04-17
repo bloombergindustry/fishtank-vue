7c1f033055fb901d4e0efe65f5f31822f0f26956
Brian Akpa <ba105445@bgov.com>
2019-04-17 09:42:38 -0400

    Remove deplication dependancy imports

--- src/components/FishTankRadio.vue
+++ src/components/FishTankRadio.vue
@@ -132,7 +132,6 @@ export default Vue.extend({
 </script>
 <style lang="scss">
   @import '../styles/mixins';
-  @import "../../node_modules/@fishtank/colors/dist/index";
   body.user-is-tabbing .a11yInput:focus {
     & + .a11yIcon{
       box-shadow: 0 0 0 2px $color-selected;
@@ -142,8 +141,6 @@ export default Vue.extend({
 
 <style module lang="scss">
   @import '../styles/mixins';
-  @import "../../node_modules/@fishtank/colors/dist/index";
-  @import "../../node_modules/@fishtank/type/dist/index";
 /* fix box sizing bug*/
   .radio{
     font-family: $font-primary;
