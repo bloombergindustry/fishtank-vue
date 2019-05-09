c7d96c8744c2d1c1bc8d82ad19e929fbe68c6762
Brian Akpa <ba105445@bgov.com>
2019-05-09 15:01:11 -0400

    fix: removed 'types/' from gitignore

---
+++ src/util/types/boxTypes.ts
@@ -0,0 +1,13 @@
+export type IsBoxType = "div" | "span" | "section" | "article" | "aside" | "footer" | "header" | "details" | "figcaption" | "figure" | "main" | "nav" | "summary" | "time";
+export type AlignItemType = "start" | "end" | "center" | "baseline" | "stretch";
+export type Direction = "row" | "column"| "row-reverse" | "column-reverse";
+export type DisplayType = "none"| "flex"| "block"| "inlineBlock"| "visuallyHidden";
+export type JustifyContent = "start" | "end" | "center" | "between" | "around";
+export type Position = 'absolute' | 'relative' | 'fixed';
+export type OverFlow = 'visible' | 'hidden' | 'scroll' | 'scrollX' | 'scrollY' | 'auto'
+export type Flex = 'grow' | 'shrink' | 'none'
+
+export type Column = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+export type Margin = -12 | -11 | -10 | -9 | -8 | -7 | -6 | -5 | -4 | -3 | -2 | -1 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13;
+export type Padding = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+export type Color = any 
