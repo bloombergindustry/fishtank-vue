0ea8088c6b60578426d3d04abe1b3673483cf54e
Amirah Chamble <Amirah.Chamble@users.noreply.github.com>
2019-06-19 15:24:02 -0400

    feat: setup for tooltips component

--- dev/views/Tooltip.vue
+++ dev/views/Tooltip.vue
@@ -1,17 +1,93 @@
 <template>
-  <section>
-    <FishTankTooltip/>
-  </section>
+  <box 
+    type="section" 
+    alignItems="center" 
+    :padding="4"
+  >
+    <box
+      type="div"
+      alignItems="center"
+    >
+
+      <FishTankHeading
+        color="black"
+        font="accent"
+        level="1"
+        size="headingMd"
+        align="center"
+        class="title"
+      >
+        FishTank Tooltip Component
+      </FishTankHeading>
+
+      <FishTankText 
+        color="black"
+        font="accent"
+        font-size="base-sm"
+        italic
+        class="title"
+      >
+        Hover over text to see Tooltips 
+      </FishTankText>
+    </box>
+
+    <!-- <FishTankTooltip/> -->
+
+    <br/>
+    <br/>
+    <br/>
+
+    <box
+      type="div"
+      color="white"
+      :padding="6"
+      display="flex"
+      flex-direction="row"
+      justify-content="between"
+    >
+      <FishTankTooltip>
+        <FishTankText color="link" semiBold
+        > Top 
+        </FishTankText>
+      </FishTankTooltip>
+
+      <FishTankTooltip>
+        <FishTankText color="link" semiBold
+        > Left 
+        </FishTankText>
+      </FishTankTooltip>
+
+      <FishTankTooltip>
+        <FishTankText color="link" semiBold
+        > Right 
+        </FishTankText>
+      </FishTankTooltip>
+
+      <FishTankTooltip>
+        <FishTankText color="link" semiBold
+        > Bottom 
+        </FishTankText>
+      </FishTankTooltip>
+
+
+    </box>
+  </box>
 </template>
 
 <script lang="ts">
-import { 
+import {
+  FishTankBox as box,
+  FishTankText,
+  FishTankHeading, 
   FishTankTooltip
 } from '@/index'
 
 import Vue from 'vue'
 export default Vue.extend({
   components:{
+    box,
+    FishTankText,
+    FishTankHeading,
     FishTankTooltip
   },
   data(){
@@ -23,6 +99,8 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-
+.title{
+  text-align: center;
+}
 </style>
 
--- src/components/FishTankTooltip.vue
+++ src/components/FishTankTooltip.vue
@@ -1,11 +1,24 @@
 <template>
-    <h1>I am a Tooltip</h1>
+    <div>
+        <slot/>
+    </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+
+import Popper from 'popper.js'
+
+import {
+    FishTankText
+} from '@/index'
+
 export default Vue.extend({
-    
+    name: 'FishTankTooltip',
+    components: {
+        FishTankText
+    },
+    props: {}
 })
 </script>
 
