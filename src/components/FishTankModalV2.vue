74c951c7ccd1d4a6eb5b6647919fd1375523b46e
Villalobos <rv110924@bna.com>
2019-07-09 11:20:26 -0400

    Refactored to use property-decorator syntax and added inline comments

--- src/components/FishTankModalV2.vue
+++ src/components/FishTankModalV2.vue
@@ -26,12 +26,14 @@
               <div 
                 class="modal__heading-icon" 
                 tabindex="-1">
+                <!-- @slot Slot for heading icon -->
                 <slot name="headingIcon"/>
               </div>
             </div>
             <div
               class="modal__heading-title"
             >
+              <!-- @slot Slot for heading -->
               <slot name="heading"/>
             </div>
           </div>
@@ -39,6 +41,7 @@
             v-if="$slots.headingExtra"
             class="modal__heading-extra"
           >
+            <!-- @slot Slot for extra heading -->
             <slot 
               name="headingExtra"/>
           </div>
@@ -60,6 +63,7 @@
         </div>
 
         <div class="modal__body">
+          <!-- @slot Slot for modal body -->
           <slot/>
         </div>
 
@@ -71,6 +75,7 @@
             v-if="$slots.footer"
             class="modal__footer-container"
           >
+          <!-- @slot Slot for footer -->
             <slot name="footer"/>
           </div>
           <div
@@ -78,9 +83,11 @@
             class="modal__footer-container"
           >
             <div class="modal__footerLeft">
+              <!-- @slot Slot for footer left -->
               <slot name="footerLeft"/>
             </div>
             <div class="modal__footerRight">
+              <!-- @slot Slot for footer right -->
               <slot name="footerRight"/>
             </div>
           </div>
@@ -91,328 +98,373 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Component, Prop, Model, Vue, Watch } from 'vue-property-decorator'
 import Detachable from '../util/detachable'
 import { Close24 } from '@fishtank/icons-vue'
+import { mixins } from 'vue-class-component';
 
 let overlayTimeout: null | number = null
 let overlay: null | HTMLElement = document.querySelector('.overlay')
 
-export default Vue.extend({
+@Component({
   components: {
     Close24
-  },
-  mixins: [
-    Detachable,
-  ],
-  model: {
-    prop: 'active',
-    event: 'change'
-  },
-  props: {
-    heading: {
-      type: String,
-      required: false,
-      default: undefined,
-      description: "Modal heading text"
-    },
-    fixed: {
-      type: Boolean,
-      required: false,
-      default: false,
-      description: "Modal has a fixed of it's maximum possible size in the viewport"
-    },
-    dialog: {
-      type: Boolean,
-      required: false,
-      default: false,
-    },
-    active: {
-      type: Boolean,
-      required: true,
-      description: "Modal is active"
-    },
-    escapeable: {
-      type: Boolean,
-      required: false,
-      default: false,
-      description: "Modal can be closed using the ESC key"
-    },
-    width: {
-      type: String,
-      required: false,
-      default: undefined,
-      description: "Modal width"
-    },
-  },
-  data() {
-    return {
-      overlayTransitionDuration: 500 + 150, // transition + delay
-      isActive: false,
+  }
+})
+export default class FishTankModalV2 extends mixins(Detachable) {
+  /**
+   * Modal is active
+   */
+  @Model('change', { type: Boolean, required: true}) active: boolean
+
+  /**
+   * Modal heading text 
+   */
+  @Prop({
+    type:String,
+    required:false,
+    default: undefined
+  })
+  heading:String
+
+  /**
+   * Modal has a fixed of it's maximum possible size in the viewport
+   */
+  @Prop({
+    type:Boolean,
+    required:false,
+    default:false
+  })
+  fixed:boolean
+
+  /**
+   * Dialog exists
+   */
+  @Prop({
+    type:Boolean,
+    required:false,
+    default:false
+  })
+  dialog:boolean
+
+  /**
+   * Modal can be closed using the ESC key
+   */
+  @Prop({
+    type:Boolean,
+    required:false,
+    default:false,
+  })
+  escapeable:boolean
+
+  /**
+   * Modal width
+   */
+  @Prop({
+    type:String,
+    required: false,
+    default: undefined
+  })
+  width:string
+
+  // Data
+  overlayTransitionDuration = 650 // transition (500) + delay (150)
+  isActive = false
+
+  // Computed functions
+  get showFooter() : Boolean {
+    return this.fixed ||
+            !!this.$slots.footer ||
+            !!this.$slots.footerRight ||
+            !!this.$slots.footerLeft
+  }
+  
+  get styles() : Record<string, string> {
+    let styles : Record<string, string> = {}
+
+    if (this.width) {
+      styles['max-width'] = this.width
     }
-  },
-  computed: {
-    showFooter() : Boolean {
-      return this.fixed ||
-             !!this.$slots.footer ||
-             !!this.$slots.footerRight ||
-             !!this.$slots.footerLeft
-    },
-    styles() : Record<string, string> {
-      let styles : Record<string, string> = {}
 
-      if (this.width) {
-        styles['max-width'] = this.width
-      }
-
-      return styles
-    },
-    classObj() : Record<any, any> {
-      let classObj : Record<string, boolean> = {
-        'modal--active': this.active,
-        'modal--fixed': this.fixed,
-        'modal--dialog': this.dialog,
-      }
-
-      // This is a bit of a hack, but since our rendered modal doesn't actually appear
-      // as a child of the top-level in the DOM, we can't simply allow a user to apply
-      // a custom class and have it take effect on the modal styling.  This copies any
-      // custom classes from the top-level item onto the actual modal div.
-      if (this.$el) {
-        let parentClasses = this.$el.className.split(' ').forEach((classVal) => {
-          if(classVal !== "modal__entry-point") {
-            classObj[classVal] = true
-          }
-        })
-      }
-
-      return classObj
+    return styles
+  }
+  get classObj() : Record<any, any> {
+    let classObj : Record<string, boolean> = {
+      'modal--active': this.active,
+      'modal--fixed': this.fixed,
+      'modal--dialog': this.dialog,
     }
-  },
-  watch: {
-    active(value : Boolean) {
-      this.updateState()
+
+    // This is a bit of a hack, but since our rendered modal doesn't actually appear
+    // as a child of the top-level in the DOM, we can't simply allow a user to apply
+    // a custom class and have it take effect on the modal styling.  This copies any
+    // custom classes from the top-level item onto the actual modal div.
+    if (this.$el) {
+      let parentClasses = this.$el.className.split(' ').forEach((classVal) => {
+        if(classVal !== "modal__entry-point") {
+          classObj[classVal] = true
+        }
+      })
     }
-  },
+
+    return classObj
+  }
+
+   @Watch('active')
+   change(value : Boolean) {
+     this.updateState()
+   }
+
   mounted() {
     this.updateState()
-  },
-  methods: {
-    close() {
-      this.$emit('change', false)
-    },
-    open() {
-      this.$emit('change', true)
-    },
-    show() {
-      if (this.escapeable) {
-        document.addEventListener('keyup', this.escapeListener)
-      }
+  }
 
-      this.isActive = true
-      this.genOverlay()
-    },
-    hide() {
-      if (this.escapeable) {
-        document.removeEventListener('keyup', this.escapeListener)
-      }
+  // Component methods
+  close() {
+    /**
+    * Change event.
+    *
+    * @type {Boolean}
+    */
+    this.$emit('change', false)
+  }
+  open() {
+    /**
+    * Change event.
+    *
+    * @type {Boolean}
+    */
+    this.$emit('change', true)
+  }
+  show() {
+    if (this.escapeable) {
+      document.addEventListener('keyup', this.escapeListener)
+    }
+    this.isActive = true
+    this.genOverlay()
+  }
+  hide() {
+    if (this.escapeable) {
+      document.removeEventListener('keyup', this.escapeListener)
+    }
 
-      // Don't remove the overlay unless this modal is currently active
-      if (this.isActive) {
-        this.isActive = false
-        this.removeOverlay()
-      }
-    },
-    updateState() {
-      if (this.active) {
-        this.show()
-      } else {
-        this.hide()
-      }
-    },
-    genOverlay() : Boolean {
-      // If fn is called and timeout is active
-      // or overlay already exists
-      // cancel removal of overlay and re-add active
-      if (overlayTimeout || overlay) {
-        this.clearTimeout()
+    // Don't remove the overlay unless this modal is currently active
+    if (this.isActive) {
+      this.isActive = false
+      this.removeOverlay()
+    }
+  }
+  updateState() {
+    if (this.active) {
+      this.show()
+    } else {
+      this.hide()
+    }
+  }
+  genOverlay() : Boolean {
+    // If fn is called and timeout is active
+    // or overlay already exists
+    // cancel removal of overlay and re-add active
+    if (overlayTimeout || overlay) {
+      this.clearTimeout()
 
-        return overlay ?
-                !!overlay.classList.add('overlay--active') :
-                false
-      }
-
-      if (!overlay) {
-        overlay = document.querySelector('.overlay') as HTMLElement | null
-      }
-      if (!overlay) {
-        overlay = document.createElement('div')
-      }
-      overlay.className = 'overlay'
-
-      this.hideScroll()
-
-      document.body.appendChild(overlay)
-
-      // eslint-disable-next-line no-unused-expressions
-      overlay.clientHeight // Force repaint
-      requestAnimationFrame(() => {
-        if (!overlay) return
-        overlay.className += ' overlay--active'
-      })
-
-      return true
-    },
-    removeOverlay () {
-      if (!overlay) {
-        return this.showScroll()
-      }
-
-      overlay.classList.remove('overlay--active')
-
-      overlayTimeout = window.setTimeout(() => {
-        // IE11 Fix
-        try {
-          if (overlay && overlay.parentNode) {
-            overlay.parentNode.removeChild(overlay)
-          }
-          overlay = null
-          this.showScroll()
-          this.clearTimeout()
-        } catch (e) {
-          // eslint-disable-next-line no-console
-          console.log(e)
-        }
-      }, this.overlayTransitionDuration)
-    },
-    clearTimeout() {
-      if (overlayTimeout) {
-        clearTimeout(overlayTimeout)
-        overlayTimeout = null
-      }
-    },
-    hideScroll () {
-      window.addEventListener('wheel', this.scrollListener)
-      window.addEventListener('keydown', this.scrollListener)
-    },
-    showScroll () {
-      document.documentElement.classList.remove('overflow-y-hidden')
-      window.removeEventListener('wheel', this.scrollListener)
-      window.removeEventListener('keydown', this.scrollListener)
-    },
-    escapeListener(e : KeyboardEvent) {
-      if(e.keyCode === 27) {
-        this.close()
-      }
-    },
-    scrollListener (e : any) {
-      if (e.type === 'keydown') {
-        if (['INPUT', 'TEXTAREA', 'SELECT'].indexOf(e.target.tagName) >= 0) return
-
-        const keyCodes = {
-          up: 38,
-          down: 40,
-          pageup: 33,
-          pagedown: 34
-        }
-
-        const up = [keyCodes.up, keyCodes.pageup]
-        const down = [keyCodes.down, keyCodes.pagedown]
-
-        if (up.indexOf(e.keyCode) >= 0) {
-          e.deltaY = -1
-        } else if (down.indexOf(e.keyCode) >= 0) {
-          e.deltaY = 1
+      //  return overlay ?
+      //          !!overlay.classList.add('overlay--active') :
+      //          false
+      // not pretty
+      if(overlay) {
+        var len = overlay.classList.length
+        overlay.classList.add('overlay--active')
+        if(overlay.classList.length > len) {
+          return true;
         } else {
-          return
+          return false;
         }
       }
+    }
 
-      if (e.target === overlay ||
-        (e.type !== 'keydown' && e.target === document.body) ||
-        this.checkPath(e)) e.preventDefault()
-    },
-    /**
-     * Polyfill for Event.prototype.composedPath
-     * @param {Event} e
-     * @returns Element[]
-     */
-    composedPath (e : any) {
-      if (e.composedPath) return e.composedPath()
+    if (!overlay) {
+      overlay = document.querySelector('.overlay') as HTMLElement | null
+    }
+    if (!overlay) {
+      overlay = document.createElement('div')
+    }
+    overlay.className = 'overlay'
 
-      const path = []
-      let el = e.target
+    this.hideScroll()
 
-      while (el) {
-        path.push(el)
+    document.body.appendChild(overlay)
 
-        if (el.tagName === 'HTML') {
-          path.push(document)
-          path.push(window)
+    // eslint-disable-next-line no-unused-expressions
+    overlay.clientHeight // Force repaint
+    requestAnimationFrame(() => {
+      if (!overlay) return
+      overlay.className += ' overlay--active'
+    })
 
-          return path
+    return true
+  }
+
+  removeOverlay () {
+    if (!overlay) {
+      return this.showScroll()
+    }
+
+    overlay.classList.remove('overlay--active')
+
+    overlayTimeout = window.setTimeout(() => {
+      // IE11 Fix
+      try {
+        if (overlay && overlay.parentNode) {
+          overlay.parentNode.removeChild(overlay)
         }
-
-        el = el.parentElement
+        overlay = null
+        this.showScroll()
+        this.clearTimeout()
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e)
       }
-    },
-        /**
-     * @param {Event} e
-     * @returns void
-     */
-    hasScrollbar (el : any) {
-      if (!el || el.nodeType !== Node.ELEMENT_NODE) return false
+    }, this.overlayTransitionDuration)
+  }
 
-      const style : any = window.getComputedStyle(el)
-      return ['auto', 'scroll'].indexOf(style['overflow-y']) >= 0 && el.scrollHeight > el.clientHeight
-    },
-    shouldScroll (el : any, delta : any) {
-      if (el.scrollTop === 0 && delta < 0) return true
-      return el.scrollTop + el.clientHeight === el.scrollHeight && delta > 0
-    },
-    isInside (el : any, parent : any) : Boolean {
-      if (el === parent) {
-        return true
-      } else if (el === null || el === document.body) {
-        return false
-      } else {
-        return this.isInside(el.parentNode, parent)
-      }
-    },
-    /**
-     * @param {Event} e
-     * @returns boolean
-     */
-    checkPath (e : any) {
-      const path = e.path || this.composedPath(e)
-      const delta = e.deltaY || -e.wheelDelta
+  clearTimeout() {
+    if (overlayTimeout) {
+      clearTimeout(overlayTimeout)
+      overlayTimeout = null
+    }
+  }
 
-      if (e.type === 'keydown' && path[0] === document.body) {
-        const dialog = this.$refs.dialog
-        const selected = window.getSelection().anchorNode
-        if (this.hasScrollbar(dialog) && this.isInside(selected, dialog)) {
-          return this.shouldScroll(dialog, delta)
-        }
-        return true
-      }
+  hideScroll () {
+    window.addEventListener('wheel', this.scrollListener)
+    window.addEventListener('keydown', this.scrollListener)
+  }
 
-      for (let index = 0; index < path.length; index++) {
-        const el = path[index]
+  showScroll () {
+    document.documentElement.classList.remove('overflow-y-hidden')
+    window.removeEventListener('wheel', this.scrollListener)
+    window.removeEventListener('keydown', this.scrollListener)
+  }
 
-        if (el === document) return true
-        if (el === document.documentElement) return true
-        if (el === this.$refs.content) return true
-
-        if (this.hasScrollbar(el)) return this.shouldScroll(el, delta)
-      }
-
-      return true
-    },
-    accessibilityClick(){
+  escapeListener(e : KeyboardEvent) {
+    if(e.keyCode === 27) {
       this.close()
     }
   }
-})
+  scrollListener (e : any) {
+    if (e.type === 'keydown') {
+      if (['INPUT', 'TEXTAREA', 'SELECT'].indexOf(e.target.tagName) >= 0) return
+
+      const keyCodes = {
+        up: 38,
+        down: 40,
+        pageup: 33,
+        pagedown: 34
+      }
+
+      const up = [keyCodes.up, keyCodes.pageup]
+      const down = [keyCodes.down, keyCodes.pagedown]
+
+      if (up.indexOf(e.keyCode) >= 0) {
+        e.deltaY = -1
+      } else if (down.indexOf(e.keyCode) >= 0) {
+        e.deltaY = 1
+      } else {
+        return
+      }
+    }
+
+    if (e.target === overlay ||
+      (e.type !== 'keydown' && e.target === document.body) ||
+      this.checkPath(e)) e.preventDefault()
+  }
+
+  /**
+   * Polyfill for Event.prototype.composedPath
+   * @param {Event} e
+   * @returns Element[]
+   */
+  composedPath (e : any) {
+    if (e.composedPath) return e.composedPath()
+
+    const path = []
+    let el = e.target
+
+    while (el) {
+      path.push(el)
+
+      if (el.tagName === 'HTML') {
+        path.push(document)
+        path.push(window)
+
+        return path
+      }
+
+      el = el.parentElement
+    }
+  }
+
+  /**
+   * @param {Event} e
+   * @returns void
+   */
+  hasScrollbar (el : any) {
+    if (!el || el.nodeType !== Node.ELEMENT_NODE) return false
+
+    const style : any = window.getComputedStyle(el)
+    return ['auto', 'scroll'].indexOf(style['overflow-y']) >= 0 && el.scrollHeight > el.clientHeight
+  }
+
+  shouldScroll (el : any, delta : any) {
+    if (el.scrollTop === 0 && delta < 0) return true
+    return el.scrollTop + el.clientHeight === el.scrollHeight && delta > 0
+  }
+
+  isInside (el : any, parent : any) : Boolean {
+    if (el === parent) {
+      return true
+    } else if (el === null || el === document.body) {
+      return false
+    } else {
+      return this.isInside(el.parentNode, parent)
+    }
+  }
+
+  /**
+   * @param {Event} e
+   * @returns boolean
+   */
+  checkPath (e : any) {
+    const path = e.path || this.composedPath(e)
+    const delta = e.deltaY || -e.wheelDelta
+
+    if (e.type === 'keydown' && path[0] === document.body) {
+      const dialog = this.$refs.dialog
+      const selected = window.getSelection().anchorNode
+      if (this.hasScrollbar(dialog) && this.isInside(selected, dialog)) {
+        return this.shouldScroll(dialog, delta)
+      }
+      return true
+    }
+
+    for (let index = 0; index < path.length; index++) {
+      const el = path[index]
+
+      if (el === document) return true
+      if (el === document.documentElement) return true
+      if (el === this.$refs.content) return true
+
+      if (this.hasScrollbar(el)) return this.shouldScroll(el, delta)
+    }
+
+    return true
+  }
+
+  accessibilityClick(){
+    this.close()
+  }
+}
+
 </script>
 
 <style scoped lang="scss">
